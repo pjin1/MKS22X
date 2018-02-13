f5e44bdd4a702937ea19c89502d562b4fc18cd4c
pjin1 <pjin1@stuy.edu>
2018-02-12 23:55:32 -0500

    addQueen, removeQueen, solve up >:(((

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -11,28 +11,43 @@ public class QueenBoard {
 		if (board[r][c] != 0) {
 			return false;
 		}
+		// -1 : Queen
+		//  0 : open spot
+		// whole numbers : # of queens threatening a spot
+		
 		//set place as Queen
-		//-1:Queen
-		//0:open spot
-		//whole numbers:# of queens threatening a spot
 		board[r][c] = -1;
+		
 		//check if valid spot
-		for (int i=0 ; i<board.length ; i++) {
-			if(isOnBoard()){			
-				board[r+i][c+i]+=1;
-				board[r-i][c+i]+=1;
-				board[r][c+i]+=1;
-				board[r+i][c]+=1;
-				board[r-i][c]+=1;
-			}
+		for (int i=1 ; i<board.length ; i++) {
+			if(isOnBoard(r+i,c+i)) {board[r+i][c+i]+=1;}
+			if(isOnBoard(r-i,c+i)) {board[r-i][c+i]+=1;}
+			if(isOnBoard(r,c+i)) {board[r][c+i]+=1;}
+			if(isOnBoard(r+i,c)) {board[r+i][c]+=1;}
+			if(isOnBoard(r-i,c)) {board[r-i][c]+=1;}
+			return true;
 		}
+		return false;
 	}
+
 	private boolean isOnBoard(int r, int c){
-		if(-1<r<board
+		return (-1<r && r<board.length) && (-1<c && c<board.length);
 	}
 
 	private boolean removeQueen(int r, int c) {
 		
+		board[r][c] = 0;
+		
+		//check if valid spot
+		for (int i=1 ; i<board.length ; i++) {
+			board[r+i][c+i]-=1;
+			board[r-i][c+i]-=1;
+			board[r][c+i]-=1;
+			board[r+i][c]-=1;
+			board[r-i][c]-=1;
+			return true;
+		}
+		return false;
 	}
 	
 	
@@ -70,7 +85,25 @@ public class QueenBoard {
 	}
 	
 	public int countSolutions(){
-		return 1;
+		for(int r=0 ; r<board.length ; r++){
+		    for(int c=0; c<board.length ; c++){
+				if(board[r][c] != 0){
+				    throw new IllegalStateException();
+				}
+		    }
+		}
+		return countSolutionsHelp();
 	}
+	
+	public int countSolutionsHelp() {
+		
+	}
+
+	public static void main(String[] ans){
+	    QueenBoard b = new QueenBoard(4);
+	    System.out.println(b.addQueen(0,0));
+	    System.out.println(b.addQueen(1,0));
+	    System.out.println(b);
+	  }
   
 }
