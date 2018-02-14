3565d5067371215d147579e4f2d962f8fc887338
pjin1 <pjin1@stuy.edu>
2018-02-13 20:00:25 -0500

    some debugs

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -23,11 +23,8 @@ public class QueenBoard {
 			if(isOnBoard(r+i,c+i)) {board[r+i][c+i]+=1;}
 			if(isOnBoard(r-i,c+i)) {board[r-i][c+i]+=1;}
 			if(isOnBoard(r,c+i)) {board[r][c+i]+=1;}
-			if(isOnBoard(r+i,c)) {board[r+i][c]+=1;}
-			if(isOnBoard(r-i,c)) {board[r-i][c]+=1;}
-			return true;
 		}
-		return false;
+		return true;
 	}
 
 	private boolean isOnBoard(int r, int c){
@@ -35,19 +32,15 @@ public class QueenBoard {
 	}
 
 	private boolean removeQueen(int r, int c) {
-		
 		board[r][c] = 0;
 		
 		//check if valid spot
 		for (int i=1 ; i<board.length ; i++) {
-			board[r+i][c+i]-=1;
-			board[r-i][c+i]-=1;
-			board[r][c+i]-=1;
-			board[r+i][c]-=1;
-			board[r-i][c]-=1;
-			return true;
+			if(isOnBoard(r+i,c+i)) {board[r+i][c+i]-=1;}
+			if(isOnBoard(r-i,c+i)) {board[r-i][c+i]-=1;}
+			if(isOnBoard(r,c+i)) {board[r][c+i]-=1;}
 		}
-		return false;
+		return true;
 	}
 	
 	
@@ -68,17 +61,29 @@ public class QueenBoard {
 		return string;
 	}
 	
-	public boolean solve(int col){
+	public boolean solve() {
+		for (int i = 0; i < board.length; i++){
+			for (int j = 0; j < board.length; j++){
+				if (board[i][j] != 0){
+					throw new IllegalStateException();
+		        }
+		    }
+		}
+		return solveHelp(0);
+	}
+	
+	public boolean solveHelp(int col){
 		if (col == board.length){
 			return true;
 		}
 		for (int r=0 ; r<board.length ; r++){
-			addQueen(r,col);
-			if (solve(col+1)) {
-				return true;
-			}
-			else {
-				removeQueen(r,col);
+			if(addQueen(r,col)) {
+				if (solveHelp(col+1)) {
+					return true;
+				}
+				else {
+					removeQueen(r,col);
+				}
 			}
 		}		
 		return false;
@@ -108,6 +113,38 @@ public class QueenBoard {
 		}
 		return sol;
 	}
+	public static void main(String[] ans){
+	    QueenBoard b = new QueenBoard(4);
 
+	    System.out.println(b.solve()); //prints true
+	    System.out.println(b); //prints a valid solution
+
+	    try{
+	      b.solve();
+	    }catch(IllegalStateException e){
+	      System.out.println("Error: The board contains non-zero values");
+	    } //prints "Error: The board contains non-zero values"
+
+	    try{
+	      b.countSolutions();
+	    }catch(IllegalStateException e){
+	      System.out.println("Error: The board contains non-zero values");
+	    } //prints "Error: The board contains non-zero values"
+
+	    for (int i = 0; i < 12; i++){
+	      QueenBoard a = new QueenBoard(i);
+	      System.out.println("# of Solutions for " + i + ": " + a.countSolutions());
+	      /*          Expected Values
+	      i --> # of Solutions   i --> # of Solutions
+	      0 --> 1                6 --> 4
+	      1 --> 1                7 --> 40
+	      2 --> 0                8 --> 92
+	      3 --> 0                9 --> 352
+	      4 --> 2               10 --> 724
+	      5 --> 10              11 --> 2680
+	      */
+	      System.out.println(a); //Should print out an empty i by i grid of underscores
+	    }
+	  }
   
 }
