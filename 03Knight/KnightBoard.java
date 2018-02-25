02c6b965bbd67f604059a9f23a4b7e23de6e0923
pjin1 <pjin1@stuy.edu>
2018-02-25 02:53:56 -0500

    lets get solving :o

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,63 +1,96 @@
 public class KnightBoard {
 	
 	private int[][] board;
-	
-	//@throws IllegalArgumentException when either parameter is negative.
+	private int[][] move = new int[][] {{-1,-2}, {-1,2}, {1,-2}, {1,2}, {-2,-1}, {-2,1}, {2,-1}, {2,1}};
+
 	public KnightBoard(int startingRows, int startingCols) {
-		if(startingRows<0 || startingCols<0) {
+		//@throws IllegalArgumentException when either parameter is negative.
+		if (startingRows<0 || startingCols<0) {
 			throw new IllegalArgumentException;
 		}
 		
 		board = new int[startingRows][startingCols];
-		
-		for (int r=0 ; r<board.length ; r++) {
-			for (int c=0 ; c<board.length ; c++) {
-				board[r][c] = 0;
-			}
-		}
+		//Initialize the board to the correct size and make them all 0's 
 	}
 	 
-	//Initialize the board to the correct size and make them all 0's 
-
 
 	public String toString() {
 		String string = ""; 
-		for (int r=0 ; r<board.length ; r++) {
-		    for (int c=0 ; c<board.length ; c++) {
 				
-		    }
-		    string += "\n";
+		for (int r=0 ; r<board.length ; r++) {
+			for (int c=0 ; c<board[0].length ; c++) {
+				if (board[r][c] == 0) {
+					ans += " _"
+				}
+				else if (board[r][c] % 10 == board[r][c]) {
+					string += "  "+board[r][c];
+				}
+				else {
+					string += " "+board[r][c];
+				}
+			}
+			string += "\n";
 		}
+		
 		return string;
 	}
-	/*
-	see format for toString below
-	blank boards display 0's as underscores 
-	you get a blank board if you never called solve or 
-	when there is no solution 
 
-	@throws IllegalStateException when the board contains non-zero values.
-	@throws IllegalArgumentException when either parameter is negative 
-	 or out of bounds. 
-	 */
 	public boolean solve(int startingRow, int startingCol) {
-		if() {
-			throw new IllegalStateException;
+		//@throws IllegalStateException when the board contains non-zero values.
+		for (int i = 0; i < board.length; i++){
+			for (int j = 0; j < board[i].length; j++){
+				if (board[i][j] != 0){
+		          throw new IllegalStateException();
+		        }
+		    }
 		}
-		return solveHelper(startingRow, startingCol, 1);
+		//@throws IllegalArgumentException when either parameter is negative or out of bounds.
+		if(startingRow<0 || startingCol<0) {
+			throw new IllegalStateException();
+		}
+		
+		return solveH(startingRow, startingCol, 1);
 	}
-	/*
-	@throws IllegalStateException when the board contains non-zero values. 
-	@throws IllegalArgumentException when either parameter is negative 
-	 or out of bounds.
-	 */
+	
+	private boolean solveH(int row ,int col, int level) {
+		// level is the # of the knight
+		if (level == board.length*board[0].length){
+		    board[row][col] = level;
+		    return true;
+		}
+		
+		for (int i=0; i<8 ; i++) {
+			int tempRow = row + move[i][0];
+			int tempCol = col + move[i][1];
+			if ((inRangeRow(tempRow)) && (inRangeCol(tempCol)) && (board[tempRow][tempCol] == 0)) {
+				board[row][col] = level;
+				if (solveH(tempRow,tempCol,level+1)){
+				    return true;
+				}
+				else {
+					board[row][col] = 0;
+				}
+			}
+		}
+		return false;
+	}
+	
+	private boolean inRangeRow(int n) {
+		if (n>=0 && n<board.length) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean inRangeCol(int n) {
+		if (n>=0 && n<board[0].length) {
+			return true;
+		}
+		return false;
+	}
+	
 	public int countSolutions(int startingRow, int startingCol) {
 		
 	}
-
 	
-	private boolean solveHelper(int row ,int col, int level) {
-		
-	}
-	// level is the # of the knight
 }
