d327ad28a18462e50f65c813d2f39e54a6bcc651
pjin1 <pjin1@stuy.edu>
2018-03-19 13:54:10 -0400

    tested with ivan's main

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -30,19 +30,31 @@ public class Quick {
 	}
 
 	public static void quicksort(int[] a){
-
+		if (a.length >= 2) {
+			QuickSortH(a, 0, a.length - 1);
+		}
 	}
-	
-	public static void QuickSortH(int[] a, int start, int end){
 
+	public static void QuickSortH(int[] a, int start, int end){
+		if(start<end) {
+			int x = partition(a,start,end);
+			QuickSortH(a,end,x-1);
+			QuickSortH(a,x+1,end);
+		}
 	}
 	
 	private static int partition(int[] data,int start,int end){
+		if (data.length < 2){
+		    return start;
+		}
+		
 		Random i = new Random();
 		int pi = data[start + i.nextInt(end-start+1)];
 		int s = start;
 	    start++;
 		
+	    swap(data,start,end);
+	    
 	    while (start < end+1){
 		    	if (data[start] > pi){
 		    		swap(data,start,end);
@@ -58,8 +70,7 @@ public class Quick {
 		    	}
 	    }
 	    
-	    swap(data,start,end);
-	    
+	  
 	    return end;
 	}
 	
@@ -71,6 +82,104 @@ public class Quick {
 		System.out.println("Pivot: " + tests[ans] + " Index: " + ans);
 		System.out.println("Pivot correct? " + checkPivot(tests, ans));
 		toString(tests);
+		int[] testt = {};
+		ans = partition(testt, 0, 0);
+		toString(testt);
+
+		int[] testu = {1};
+		ans = partition(testu, 0, 1);
+		System.out.println("Pivot: " + testu[ans] + " Index: " + ans);
+		toString(testu);
+
+
+		int[] testv = {1,1};
+		ans = partition(testv, 0, 1);
+		System.out.println("Pivot: " + testv[ans] + " Index: " + ans);
+		toString(testv);
+
+		int[] testw = {2,1};
+		ans = partition(testw, 0, 1);
+		System.out.println("Pivot: " + testw[ans] + " Index: " + ans);
+		toString(testw);
+
+		int[] testx = {1,2};
+		ans = partition(testx, 0, 1);
+		System.out.println("Pivot: " + testx[ans] + " Index: " + ans);
+		toString(testx);
+		System.out.println("DONE!\n");
+
+
+
+		//Borrowed most from K
+		int[] test = {1000, 999,999,999,4,1,0,3,2,999,999,100,100,-10000, 10212, -19212, 23, 12};
+		toString(test);	
+		////SHOULD PRINT THE ARRAY IN ORDER FROM LEAST TO GREATEST
+		System.out.println("\nPRINTING LEAST TO GREATEST");
+		for (int i = 0; i < test.length; i++){
+		    System.out.println(quickselect(test, i));;
+		}
+		System.out.println("DONE!\n");
+
+
+		//Testing QuickSort
+		System.out.println("TESTING QUICKSORT!");
+		//General Case
+
+		long start,end;
+		start = System.currentTimeMillis();
+
+		System.out.print("Before: ");
+		int[] testy = {1000, 999,999,999,4,1,0,3,2,999,999,100,100,-10000, 10212, -19212, 23, 12};
+		toString(testy);
+		quicksort(testy);
+		end = System.currentTimeMillis();
+
+		System.out.print("aAfter: ");
+		toString(testy);
+		System.out.println("Time: " + (end-start) + "ms");
+
+		//Edge Cases
+		System.out.print("\nTESTING EDGY CASES!\n");
+
+		int[] testz = {};
+		System.out.print("Before: ");
+		toString(testz);
+		System.out.print("aAfter: ");
+		quicksort(testz);
+		toString(testz);
+		System.out.println();
+
+		int[] testa = {1};
+		System.out.print("Before: ");
+		toString(testa);
+		System.out.print("aAfter: ");
+		quicksort(testa);
+		toString(testa);
+		System.out.println();
+
+		int[] testb = {1,1};
+		System.out.print("Before: ");
+		toString(testb);
+		System.out.print("aAfter: ");
+		quicksort(testb);
+		toString(testb);
+		System.out.println();
+
+		int[] testc = {2,1};
+		System.out.print("Before: ");
+		toString(testc);
+		System.out.print("aAfter: ");
+		quicksort(testc);
+		toString(testc);
+		System.out.println();
+
+		int[] testd = {1,2};
+		System.out.print("Before: ");
+		toString(testd);
+		System.out.print("aAfter: ");
+		quicksort(testd);
+		toString(testd);
+		System.out.println();
 	}
 	private static boolean checkPivot(int[] ary, int pivot){
 		for (int i = 0; i < pivot; i++){
