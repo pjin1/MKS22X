f27507ad409a45f84359a94aef5d8e82bc0d3e6d
pjin1 <pjin1@stuy.edu>
2018-03-19 02:42:52 -0400

    pseudo code

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,1 +1,18 @@
+pi = selectARandomIndex()
+swap (data,pi,lo)
+i=lo+1
+j=hi
+while(i<=j) {
+	if data[i]>V: //v is data[0]
+		swap(data,i,j)
+		j--
+		else
+			i++
+}
+swap(data,j,lo)
+return j
+	//THIS DOESNT ACCOUNT FOR WHEN ARRAY HAS ALL SAME VALUE, ONLY RANDOMIZED ARRAYS
 
+public class Quicksort {
+	
+}
