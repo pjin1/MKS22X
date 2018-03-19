91094b81a56eba2eba452f365923b647020f01d7
pjin1 <pjin1@stuy.edu>
2018-03-19 11:14:12 -0400

    wrote partition

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,4 +1,97 @@
-pi = selectARandomIndex()
+import java.util.*;
+import java.io.*;
+
+public class Quick {
+
+	private static void swap(int[] ary,int a, int b) {
+		int c = ary[a];
+		ary[a] = ary[b];
+		ary[b] = c;
+	}
+
+	public static int quickselect(int[] data, int k){
+		int start = 0, end = data.length-1;
+		int i = partition(data, start, end);
+
+		while(i[0] > k || i[1] < k){
+			if (i[0] < k){
+				start = i[1]+1;
+			}
+			else {
+				end = i[0]-1;
+			}
+			i = partition(data,start,end);
+		}
+
+		return data[i[0]];
+	}
+
+	public static void quicksort(int[] a){
+
+	}
+	
+	public static void QuickSortH(int[] a, int start, int end){
+
+	}
+	
+	private static int partition(int[] data,int start,int end){
+		Random i = new Random();
+		int pi = data[start + i.nextInt(end-start+1)];
+		int s = start;
+	    start++;
+		
+	    while (start < end+1){
+		    	if (data[start] > pi){
+		    		swap(data,start,end);
+		    		end--;
+		    	}
+		    	else if (data[start] < pi){
+		    		swap(data,start,s);
+		    		s++;
+		    		start++;
+		    	}
+		    	else{
+		    		start++;
+		    	}
+	    }
+	    
+	    swap(data,start,end);
+	    
+	    return end;
+	}
+	
+	public static void main(String[] args){
+		int[] tests = {1000, 999,999,999,4,1,0,3,2,999,999,100,100,-10000, 10212, -19212, 23, 12};
+
+		int ans = partition(tests, 0, tests.length - 1);
+		System.out.println("Pivot: " + tests[ans] + " Index: " + ans);
+		System.out.println("Pivot correct? " + checkPivot(tests, ans));
+		toString(tests);
+	}
+	private static boolean checkPivot(int[] ary, int pivot){
+		for (int i = 0; i < pivot; i++){
+			if (ary[i] > ary[pivot]){
+				return false;
+			}
+		}
+		for (int j = pivot + 1; j < ary.length; j++){
+			if (ary[j] < ary[pivot]){
+				return false;
+			}
+		}
+		return true;
+	}
+	private static void toString(int[] parti){
+		for (int k = 0; k < parti.length; k++){
+			System.out.print(parti[k] + ", ");
+		}
+		System.out.println();
+	}
+
+}
+
+
+/*pi = selectARandomIndex()
 swap (data,pi,lo)
 i=lo+1
 j=hi
@@ -12,7 +105,4 @@ while(i<=j) {
 swap(data,j,lo)
 return j
 	//THIS DOESNT ACCOUNT FOR WHEN ARRAY HAS ALL SAME VALUE, ONLY RANDOMIZED ARRAYS
-
-public class Quicksort {
-	
-}
+*/
