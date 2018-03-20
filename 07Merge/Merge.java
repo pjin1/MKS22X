57ab5ce08d754c9f3ae404f3e5bd18c044cf7ce0
pjin1 <pjin1@stuy.edu>
2018-03-20 14:35:00 -0400

    basics

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,8 +1,17 @@
-mergesort(data)
-temp=new int[]
-		msort(data,temp,lo,hi)
-		msort(temp,data,lo,(lo+hi)/2)
-		msort(temp,data,(lo+hi)/2 +1, hi)
+import java.util.*;
+import java.io.*;
 
-		merge(data,temp,lo,hi)
+public class Merge {
+	public static int msort() {
 		
+	}
+}
+
+/*mergesort(data)
+temp=new int[];
+msort(data,temp,lo,hi)
+msort(temp,data,lo,(lo+hi)/2)
+msort(temp,data,(lo+hi)/2 +1, hi)
+
+merge(data,temp,lo,hi)
+*/
