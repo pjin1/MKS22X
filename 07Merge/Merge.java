4b2780bdded9bcd665237876e0846c4f4776c039
PeterJin <pjin1@stuy.edu>
2018-03-22 13:42:38 -0400

    wrote msort

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -2,12 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class Merge {
-	public static void mergesort(int[] data) {
+	public static void mergesort(int[] data, int[] temp, int lo, int hi, int lo1, int hi1) {
 		
 	}
 	
 	public static void msort(int[] data, int[] temp, int lo, int hi) {
-		
+		if (lo<hi){
+			for (int i=lo; i<hi+1; i++){
+				temp[i]=data[i];
+}
+msort(temp,data,lo,(lo+hi)/2)
+msort(temp,data,(lo+hi)/2 +1, hi)
+
+merge(data,temp,lo,(lo+hi)/2,(lo+hi)/2 +1,hi)
+
 	}
 }
 
