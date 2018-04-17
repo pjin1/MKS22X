2392a503021c18bde3087b3aec106e9de0e1863a
pjin1 <pjin1@stuy.edu>
2018-04-16 22:46:48 -0400

    basics

---
+++ 08LinkedLists/MyLinkedListImproved.java
@@ -0,0 +1,13 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyLinkedListImproved<T extends Comparable<T>> implements Iterable<T>{
+	private Node start, end;
+	private int size;
+
+	public MyLinkedListImproved(){
+		
+	}
+
+	
+}
