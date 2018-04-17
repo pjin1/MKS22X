0ae407b8b5b392787097cf47524db3635fb859ce
pjin1 <pjin1@stuy.edu>
2018-04-17 07:24:04 -0400

    private class node

--- 08LinkedLists/MyLinkedListImproved.java
+++ 08LinkedLists/MyLinkedListImproved.java
@@ -9,5 +9,48 @@ public class MyLinkedListImproved<T extends Comparable<T>> implements Iterable<T
 		
 	}
 
-	
+	public void clear(){
+		first = null;
+		last = null;
+		length = 0;
+	}
+
+	private class Node{
+
+		private int data;
+		private Node prev, next;
+
+		public Node(int value){
+			data = value;
+			prev = next = null;
+		}
+
+		public Node getPrev(){
+			return prev;
+		}
+
+		public void setPrev(Node node){
+			prev = node;
+		}
+
+		public Node getNext(){
+			return next;
+		}
+
+		public void setNext(Node node){
+			next = node;
+		}
+
+		public int getValue(){
+			return data;
+		}
+
+		public void setValue(int value){
+			data = value;
+		}
+
+		public String toString(){
+			return "[" + data + "]";
+		}
+	}
 }
