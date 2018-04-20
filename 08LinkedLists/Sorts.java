bad2f25d397edc8ea53627c8a6d932935475d03c
pjin1 <pjin1@stuy.edu>
2018-04-20 01:24:35 -0400

    whyamilikethis

--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -3,9 +3,29 @@ import java.util.*;
 public class Sort extends MyLinkedListImproved{
 
 	public static void radixsort(MyLinkedListImproved<Integer> data){
+		if (data.size() <= 1){
+		    return;
+		}
+		
+		@SuppressWarnings("unchecked") MyLinkedListImproved<Integer>[] bucket = new MyLinkedListImproved[10];
+		
+		MyLinkedListImproved<Integer> posVals = new MyLinkedListImproved<>(), negVals = new MyLinkedListImproved<>();
 
+		for (Integer i : data){
+			if (i>0){
+				posVals.add(i);
+			}
+			else{
+				negVals.add(i);
+			}
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			bucket[i]= new MyLinkedListImproved<Integer>();
+		}
+		
 	}
-	
+
 	public static void radixsortIncludingNegatives(MyLinkedListImproved<Integer> data){ 
 		
 	}
