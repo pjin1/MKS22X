78ddcb3f789e7e61808a39819f55fe7238d01c81
pjin1 <pjin1@stuy.edu>
2018-04-23 02:56:12 -0400

    issues with nullpointerexception

--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
-public class Sort extends MyLinkedListImproved{
+public class Sorts extends MyLinkedListImproved{
 
 	public static void radixsort(MyLinkedListImproved<Integer> data){
+
 		if (data.size() <= 1){
 		    return;
 		}
@@ -24,10 +25,56 @@ public class Sort extends MyLinkedListImproved{
 			bucket[i]= new MyLinkedListImproved<Integer>();
 		}
 		
+		if (posVals.size() > 0){
+		    double max = posVals.max();
+		    int maxDig = (int)Math.log10(max);
+
+		    for(int i = 0; i < maxDig; i++){
+				for(Integer num : posVals){
+
+			    		int val = (int) (num / Math.pow(10, i)) % 10;
+	
+			    		bucket[val].add(num);
+				}
+		    }
+		    posVals.clear();
+		    
+		    for(int i = 0; i < 10; i++){
+		    		posVals.extend(bucket[i]);
+		    }
+		}
+		
+		if (negVals.size() > 0) {
+			double min = negVals.min();
+		    int maxDig = (int)Math.log10(min);
+
+		    for(int i = 0; i < maxDig; i++){
+				for(Integer num : negVals){
+
+			    		int val = (int) (num / Math.pow(10, i)) % 10;
+	
+			    		bucket[val].add(num);
+				}
+		    }
+		    negVals.clear();
+		    
+		    for(int i = 0; i < 10; i++){
+		    		negVals.extend(bucket[i]);
+		    }
+		}
+
+		data.clear();
+
+		for(Integer i: negVals){
+			data.add(0, -1*i);
+		}
+		
+		data.extend(posVals);
+	    
 	}
 
 	public static void radixsortIncludingNegatives(MyLinkedListImproved<Integer> data){ 
-		
+		radixsort(data);
 	}
 
 }
