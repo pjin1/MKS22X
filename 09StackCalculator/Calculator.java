060ef47762faa19a5aca2c37db64148c601b30d0
pjin1 <pjin1@stuy.edu>
2018-04-24 11:17:28 -0400

    working calculator

--- 09StackCalculator/Calculator.java
+++ 09StackCalculator/Calculator.java
@@ -62,12 +62,4 @@ public class Calculator{
 
 	}
 
-	public static void main(String [] args){
-		System.out.println(eval("10 2 +"));//12.0
-		System.out.println(eval("10 2 -"));//8.0
-		System.out.println(eval("10 2.0 +"));//12.0
-		System.out.println(eval("11 3 - 4 + 2.5 *"));//30.0
-		System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));//839.0
-		System.out.println(eval("10 2 + 10 * 1 + 1 1 1 + + + 10 10 + -"));//104.0
-	    }
-	}
+}
