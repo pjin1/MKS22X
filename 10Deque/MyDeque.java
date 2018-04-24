b04b83a60ee7e841112b8ac743265e41e4c89eb8
pjin1 <pjin1@stuy.edu>
2018-04-24 06:59:42 -0400

    all the exceptions

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,1 +1,60 @@
+import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
 
+public class MyDeque<E>{
+	
+	public MyDeque() {
+		
+	}
+	
+	public MyDeque(int initialCapacity) {
+		if (initialCapacity < 0){
+		    throw new IllegalArgumentException();
+		}
+	}
+	
+	int size() {
+		
+	}
+
+	
+	public void addFirst(E) {
+		if (e == null){
+		    throw new NullPointerException();
+		}
+	}
+	
+	public void addLast(E) {
+		if (e == null){
+		    throw new NullPointerException();
+		}
+	}
+
+	public E removeFirst() {
+		if(size == 0){
+		    throw new NoSuchElementException();
+		}
+	}
+	
+	public E removeLast() {
+		if(size == 0){
+		    throw new NoSuchElementException();
+		}
+	}
+	
+	public E getFirst() {
+		if(size == 0){
+		    throw new NoSuchElementException();
+		}
+	}
+	
+	public E getLast() {
+		if(size == 0){
+		    throw new NoSuchElementException();
+		}
+		
+	}
+
+    	
+}
