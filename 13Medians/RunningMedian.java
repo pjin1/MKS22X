0b7270f04d0b8ce2e0cca5742a4c54b7ad6f15b5
pjin1 <pjin1@stuy.edu>
2018-05-01 16:31:29 -0400

    skeleton :o

---
+++ 13Medians/RunningMedian.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class RunningMedian {
+
+	//makes an empty container for Doubles.
+	public RunningMedian() {
+		
+	}
+	
+	//insert a Double into the data structure.
+	public void add(Double) {
+		
+	}
+	
+	//return the current median, throws NoSuchElementException when size is 0.
+	public Double getMedian() {
+		
+	}
+			
+	public int size() {
+		
+	}
+	
+}
