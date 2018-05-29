0453f9e5b6c35c494d64fb6c7d1d2176365d47d9
pjin1 <pjin1@stuy.edu>
2018-05-29 01:23:26 -0400

    not tested yet

--- 12MyHeap/MyHeap.java
+++ 12MyHeap/MyHeap.java
@@ -7,44 +7,125 @@ public class MyHeap<T extends Comparable<T>>{
 	
 	@SuppressWarnings("unchecked")
 	public MyHeap(){
-		data = (T[])new Comparable[10];
+		heap = (T[])new Comparable[10];
+		
 		max = true;
 		size = 0;
 	}
 	
 	@SuppressWarnings("unchecked")
 	public MyHeap(boolean max){
+		heap = (T[]) new Comparable[10];
+
 		max = this.max;
 		size = 0;
-		heap = (T[]) new Comparable[10];
 	}
 
 	public int size(){
-		return length;
+		return size;
 	}
 
 	public T peek(){
-		return arr[0];
+		return heap[0];
 	}
 	
 	@SuppressWarnings("unchecked")
 	private void resize(){
-		T[] temp = (T[]) new Comparable[heap.length * 2];
-		
-		for (int i=0; i<heap.length; i++){
-			temp[i] = heap[i];
+		T[] resized = (T[]) new Comparable[heap.length * 2];
+		int counter = 0;
+		 
+		while (counter<heap.length) {
+			resized[counter] = heap[counter];
+			
+			counter++;
 		}
 		
-		heap = temp;
+		heap = resized;
 	}
 
 	
 	public void add(T elem) {
+		if (size == heap.length){
+			resize();
+		}
+		heap[size] = elem;
 		
+		up(size); 
+		size += 1;
 	}
 	
 	public T remove() {
+		T ans = heap[0];
+	    heap[0] = heap[size-1];
+	    
+	    down(0);
+	    
+	    heap[size-1] = null;
+	    size-=1;
+	    
+	    return ans;
+	}
+	
+	private void swap(T[] temp,int a, int b) {
+		T c = temp[a];
+		temp[a] = temp[b];
+		temp[b] = c;
+	}
+	
+	private void up(int ind) {
+		int pos = (ind-1)/2;
+		T low = heap[ind];
+
+        while (max && (pos>=0) && (heap[ind].compareTo(heap[pos]))>0){
+        		swap(heap,pos,ind);
+        }
+        		
+	}
+	
+	private void down(int ind) {
+		int pos1 = 2*ind+1;
+		int pos2 = 2*ind+2;
+		
+		
+		if (max) {
+			if (((pos1 < size) && (heap[ind].compareTo(heap[pos1]) < 0)) || 
+			    ((pos2 < size) && (heap[ind].compareTo(heap[pos2]) < 0))) {
+				
+				if (heap[pos1].compareTo(heap[pos2]) > 0) {
+	                swap(heap,pos1,ind);
+	                ind = pos1;
+	                down(pos1);
+	            }
+
+	            else {
+	                swap(heap,pos2,ind);
+	                ind = pos2;
+	                down(pos2);
+	            }
+		
+			}
+		}
+		
+		else {
+			if ((pos1 < size) && (heap[ind].compareTo(heap[pos1]) > 0) ||
+			    (pos2 < size) && (heap[ind].compareTo(heap[pos2]) > 0)) {
+				
+                if (heap[pos1].compareTo(heap[pos2]) < 0) {
+                    swap(heap,pos1,ind);
+                    ind = pos1;
+                    down(pos1);
+                }
+
+                else {
+                		swap(heap,pos2,ind);
+	                ind = pos2;
+	                down(pos2);
+                }
+                
+			}
+		}
 		
 	}
 	
+								 
 }
