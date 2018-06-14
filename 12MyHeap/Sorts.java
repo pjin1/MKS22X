85d97e06b671efbe7e391c6a129ddcf5c5b54787
pjin1 <pjin1@stuy.edu>
2018-06-13 23:20:40 -0400

    nlogn runtime but at least it sorts...

--- 12MyHeap/Sorts.java
+++ 12MyHeap/Sorts.java
@@ -67,32 +67,5 @@ public class Sorts {
 	}
 	
 
-    public static void main(String[] args){
-	Integer[] d1 = new Integer[10];
-	Integer[] d2 = new Integer[10];
-	for (int i = 0; i < 10; i++){
-	    int num = (int)(Math.random() * 1000);
-	    d1[i] = num;
-	    d2[i] = num;
-	}
-
-	System.out.println(Arrays.toString(d1));
-	System.out.println(Arrays.toString(d2));
-	
-	Arrays.sort(d1);
-	heapsort(d2);
-
-	for (int i = 0; i < 10; i++){
-	    if (d1[i] != d2[i]){
-		System.out.println("error at "+ i);
-		System.out.println(Arrays.toString(d1));
-		System.out.println(Arrays.toString(d2));
-		System.exit(1);
-	    }
-	}
-
-	System.out.println("no error");
-    }
-
 
 }
